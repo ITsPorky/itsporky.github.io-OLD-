9b2249e07390aaafed96894317bdb89c1a00def4
ITsPorky <wc.walsh16@gmail.com>
2022-10-06 17:39:45 +1000

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Navbar active
+const links = document.querySelectorAll(".nav-link");
+
+if (links.length) {
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      links.forEach((link) => {
+        link.classList.remove("active");
+      });
+      e.preventDefault();
+      link.classList.add("active");
+    });
+  });
+}
